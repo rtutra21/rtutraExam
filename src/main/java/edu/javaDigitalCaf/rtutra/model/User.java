9b3a17ed32fe25381a7745f0d5ebf18b14f9b787
rtutra21 <artur2003449@gmail.com>
2025-07-07 15:33:05 +0300

    RtutraExamApplication

--- src/main/java/edu/javaDigitalCaf/rtutra/model/User.java
+++ src/main/java/edu/javaDigitalCaf/rtutra/model/User.java
@@ -1,10 +1,7 @@
 package edu.javaDigitalCaf.rtutra.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 
 @Getter
